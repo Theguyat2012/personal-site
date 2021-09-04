1c758000367403c4e07eddfbca40f6aa324f883e
Alex Ho <ho.alex100@gmail.com>
2021-09-04 01:08:45 -0400

    Add static pages

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+  end
+
+  test "should get employment" do
+    get static_pages_employment_url
+    assert_response :success
+  end
+
+  test "should get projects" do
+    get static_pages_projects_url
+    assert_response :success
+  end
+end
