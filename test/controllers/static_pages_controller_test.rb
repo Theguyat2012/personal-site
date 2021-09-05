2a83a1a4fe9f76c1fb08c62f8c21ff5c223105ee
Alex Ho <ho.alex100@gmail.com>
2021-09-04 22:49:39 -0400

    Adjust showcase descriptions

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,6 +11,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select 'title', "#{@base_title}"
     navigation_links_exist
     assert_select 'h1', 'Welcome'
+    assert_select 'h2', 'What brings you here today?'
   end
 
   test "should get about" do
